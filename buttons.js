9b3ae0144f8d4a8f9e8cff930658cff63d344050
Andy Voltz <andy.voltz@gmail.com>
2017-03-19 21:38:03 -0500

    add button pressing behavior, refactoring
    store canvas on the button class
    split setup_context for group button and wave buttons

--- buttons.js
+++ buttons.js
@@ -1,58 +1,58 @@
 class Button {
-  constructor() {
+  constructor(el, cls, height, width) {
+    this.pressed = false;
+    var new_el = document.createElement("canvas");
+    new_el.className = cls;
+    new_el.height = height;
+    new_el.width = width;
+    {
+      var self = this;
+      new_el.onclick = function() {
+        self.pressed = ! self.pressed;
+        self.element.className = self.pressed ? `${cls} pressed` : cls;
+      }
+    }
+    this.element = new_el;
+    el.appendChild(this.element);
   }
-  create_canvas(el, cls, height, width) {
-    var new_element = document.createElement("canvas");
-    new_element.className = cls;
-    new_element.height = height;
-    new_element.width = width;
-    new_element.onclick = this.onClick;
-    el.appendChild(new_element);
-    return new_element;
-  }
-  setup_context(el) {
-      var ctx = el.getContext("2d");
-      ctx.clearRect(0, 0, el.width, el.height);
+  setup_context() {
+      var ctx = this.element.getContext("2d");
+      ctx.clearRect(0, 0, this.element.width, this.element.height);
       ctx.beginPath();
       return ctx;
-  }
-  onClick() {
-    // check for data-nprn or data-cc attributes and send
-    console.log('click click ritchie');
   }
 }
 
 class GroupButton extends Button {
   constructor(el, size) {
-    super();
-    var canvas = this.create_canvas(el, 'grp_btn', size, size);
-    this.draw(canvas);
+    super(el, 'grp_btn', size, size);
+    this.draw();
   }
-  draw(el) {
-    var ctx = this.setup_context(el);
-    var mid_height = el.height / 2;
-    var mid_width = el.width / 2;
-    ctx.moveTo(1 / 8 * el.width, mid_height);
-    ctx.lineTo(7 / 8 * el.width, mid_height);
+  draw() {
+    var ctx = this.setup_context();
+    var mid_height = this.element.height / 2;
+    var mid_width = this.element.width / 2;
+    ctx.moveTo(1 / 8 * this.element.width, mid_height);
+    ctx.lineTo(7 / 8 * this.element.width, mid_height);
     ctx.stroke();
     ctx.beginPath();
-    ctx.moveTo(mid_width, 1 / 8 * el.height);
-    ctx.lineTo(mid_width, 7 / 8 * el.height);
+    ctx.moveTo(mid_width, 1 / 8 * this.element.height);
+    ctx.lineTo(mid_width, 7 / 8 * this.element.height);
     ctx.stroke();
   }
 }
 
 class WaveShapeButton extends Button {
   constructor(el, cls, height, width) {
-    super()
-    var new_element = this.create_canvas(el, cls, height, width);
-    this.draw(new_element);
+    super(el, cls, height, width)
+    this.draw();
   }
-  setup_context(el) {
-      var ctx = super.setup_context(el);
-      var height = 3 / 4 * el.height;
-      var bottom = 1 / 4 * el.height;
-      ctx.moveTo(1 / 8 * el.width, height);
+  setup_context() {
+      var ctx = super.setup_context();
+      var el = this.element;
+      var height = 3 / 4 * this.element.height;
+      var bottom = 1 / 4 * this.element.height;
+      ctx.moveTo(1 / 8 * this.element.width, height);
       return {
         ctx,
         height,
@@ -70,16 +70,16 @@ class TriangleButton extends WaveShapeButton {
     super(el, 'tri_btn', height, width);
   }
 
-  draw(el) {
+  draw() {
     var {
       ctx,
       height,
       bottom
-    } = this.setup_context(el);
-    ctx.lineTo(5 / 16 * el.width, bottom);
-    ctx.lineTo(1 / 2 * el.width, height);
-    ctx.lineTo(11 / 16 * el.width, bottom);
-    ctx.lineTo(7 / 8 * el.width, height);
+    } = this.setup_context();
+    ctx.lineTo(5 / 16 * this.element.width, bottom);
+    ctx.lineTo(1 / 2 * this.element.width, height);
+    ctx.lineTo(11 / 16 * this.element.width, bottom);
+    ctx.lineTo(7 / 8 * this.element.width, height);
     ctx.stroke();
   }
 }
@@ -88,17 +88,17 @@ class SquareButton extends WaveShapeButton {
   constructor(el, height, width) {
     super(el, 'sq_btn', height, width);
   }
-  draw(el) {
+  draw() {
     var {
       ctx,
       height,
       bottom
-    } = this.setup_context(el);
+    } = this.setup_context();
     var x_points = [3 / 16, 7 / 16, 9 / 16, 13 / 16];
     var y_next = height;
     var y_nextnext = bottom;
     x_points.forEach(x => {
-        var width = x * el.width;
+        var width = x * this.element.width;
         ctx.lineTo(width, y_next);
         ctx.lineTo(width, y_nextnext);
         // reuse width here as a place to keep y_next so we swap...
@@ -106,7 +106,7 @@ class SquareButton extends WaveShapeButton {
         y_next = y_nextnext;
         y_nextnext = width;
       })
-    ctx.lineTo(7 / 8 * el.width, height);
+    ctx.lineTo(7 / 8 * this.element.width, height);
     ctx.stroke();
   }
 }
@@ -115,15 +115,15 @@ class SawtoothButton extends WaveShapeButton {
   constructor(el, height, width) {
     super(el, 'saw_btn', height, width);
   }
-  draw(el) {
+  draw() {
     var {
       ctx,
       height,
       bottom
-    } = this.setup_context(el);
+    } = this.setup_context();
     var x_points = [5 / 16, 1 / 2, 11 / 16, 14 / 16];
     x_points.forEach(x => {
-      var width = x * el.width;
+      var width = x * this.element.width;
       ctx.lineTo(width, bottom);
       ctx.lineTo(width, height);
     })
