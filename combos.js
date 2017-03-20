f24b86e6910696e9f32c898df3271299a054df42
Andy Voltz <andy.voltz@gmail.com>
2017-03-19 22:56:50 -0500

    add simple Combo class

---
+++ combos.js
@@ -0,0 +1,24 @@
+class Combo {
+  constructor(el, labels, values, title) {
+    let container = document.createElement("div");
+    if (typeof(title) != 'undefined') {
+      let new_lbl = document.createElement("label");
+      new_lbl.innerHTML = title;
+      container.appendChild(new_lbl);
+    }
+    let new_el = document.createElement("select");
+    let i = 0;
+    if (labels.length != values.length) {
+      console.log('Combo: len of labels != values');
+    }
+    labels.forEach(label => {
+      let op = document.createElement("option");
+      op.innerHTML = label;
+      op.value = values[i++];
+      new_el.appendChild(op);
+    })
+    container.appendChild(new_el);
+    el.appendChild(container);
+    this.element = new_el;
+  }
+}
