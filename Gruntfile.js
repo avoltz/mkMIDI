115ef13168b25ce0405de7071c010aa2c0d335c5
Andy Voltz <andy.voltz@gmail.com>
2017-03-26 11:53:52 -0400

    reorganized code, added index.html pages
    Moved widgets and demo html pages to directories

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,7 +2,7 @@ module.exports = function(grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
     jshint: {
-      all : [ '*.js' ],
+      all : [ '*.js', 'widgets/*.js' ],
       options: {
         esversion: 6
       }
