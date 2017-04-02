f04f51f8445616ee31079d8b790f7d2e29398ca4
Andy Voltz <andy.voltz@gmail.com>
2017-04-02 13:27:09 -0500

    add editor class

--- widgets/editor.js
+++ widgets/editor.js
@@ -1,19 +1,69 @@
-document.addEventListener('DOMContentLoaded', function() {
-  let editor_controls = document.getElementsByClassName("editor-controls");
-  let editor_container = document.getElementsByClassName("editor-container");
-  if (editor_container.length != 1) {
-    console.log('could not find editor-container. cannot load editor');
-    return;
-  }
-  editor_container = editor_container[0];
-  let links = document.getElementsByClassName("editor-swap-view");
-  let link_click = function() {
-    for (let i = 0; i < editor_controls.length; i++) {
-      editor_controls[i].classList.toggle("right");
+class Editor {
+  constructor(id) {
+    let container = document.getElementById(id);
+    if (typeof(container) === 'undefined') {
+      console.log(`${id} not found to construct Editor`);
+      return;
     }
-    editor_container.classList.toggle("col");
-  };
-  for (let i = 0; i < links.length; i++) {
-    links[i].onclick = link_click;
+    container.className = 'editor-container';
+
+    let controls = document.createElement("div");
+    controls.className = 'editor-controls right';
+
+    let instrument_element = document.createElement("div");
+    instrument_element.className = "instrument";
+    let my_synth = {"name" : "New Synth", "sections" : []};
+    this.instrument = new Instrument(instrument_element, my_synth);
+
+    /* setup the view swap links */
+    let links = document.getElementsByClassName("editor-swap-view");
+    let link_click = function() {
+      controls.classList.toggle("right");
+      container.classList.toggle("col");
+    };
+    for (let i = 0; i < links.length; i++) {
+      links[i].onclick = link_click;
+    }
+
+    let create_input = function(name, val) {
+      let el = document.createElement("input");
+      el.type = name == 'label' ? 'text' : 'number';
+      el.placeholder = name;
+      el.value = typeof(val) === 'undefined' ? '' : val;
+      return el;
+    };
+
+    /* setup controls */
+    let widget_select = document.createElement("select");
+    for (let p in Section.params) {
+      let op = document.createElement("option");
+      op.innerHTML = p;
+      op.value = p;
+      widget_select.appendChild(op);
+    }
+    widget_select.selectedIndex = -1;
+    let control_params = document.createElement("div");
+    control_params.className = 'editor-control-params';
+    widget_select.onchange = function() {
+      while (control_params.children.length > 0) { control_params.removeChild(control_params.firstChild); }
+      let params = Section.params[widget_select.value];
+      for (let i = 0; i < params.length; i++) {
+        let e = create_input(params[i]);
+        control_params.appendChild(e);
+      }
+    };
+    controls.appendChild(widget_select);
+    let add = document.createElement('input');
+    add.type = 'button';
+    add.value = 'Add';
+    add.onclick = function() {
+      console.log('add');
+    };
+    controls.appendChild(add);
+    controls.appendChild(control_params);
+
+    /* add the elements to the page */
+    container.appendChild(instrument_element);
+    container.appendChild(controls);
   }
-});
+}
