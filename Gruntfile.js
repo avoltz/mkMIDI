f0b86bf8953ee82b0a588e6f5543b579f4c69883
Andy Voltz <andy.voltz@gmail.com>
2017-04-16 13:03:45 -0500

    add grunt-babel support

--- Gruntfile.js
+++ Gruntfile.js
@@ -9,11 +9,23 @@ module.exports = function(grunt) {
     },
     exec: {
       server: './ssl-server.sh'
+    },
+    babel: {
+      options: {
+        presets: ['env']
+      },
+      dist: {
+        files: {
+          'dist/mkmidi-instrument.js': 'instrument.js',
+          'dist/mkmidi-editor.js': 'editor.js'
+        }
+      }
     }
   });
 
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-exec');
+  grunt.loadNpmTasks('grunt-babel');
 
   grunt.registerTask('default', ['jshint']);
   grunt.registerTask('server', ['exec', 'server']);
